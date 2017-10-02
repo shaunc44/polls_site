44a6c08454b94af30d214ae7f420dd48ccc6b16b
Shaun <seyyes2000@hotmail.com>
2017-10-02 10:41:55 -0400

    change models; create admin

--- polls_site/polls_site/settings.py
+++ polls_site/polls_site/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls_app.apps.PollsAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
